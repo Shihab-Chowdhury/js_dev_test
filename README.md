a9c9416b48434ce3ee816e082a371953188ecf38
Shihab Chy <cshihab04@gmail.com>
2021-12-21 00:44:01 +0600

    test complete

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # js_dev_test
 JavaScript Developer Code Test
 
+A node module using Node NAN
+
 In this directory run:
 
-1. npm install: Install all the dependencies as well as configure and build.
+1. npm install: Will run node-gyp rebuild for compilation.
 
-2. node-gyp rebuild: For compiling 
+2. npm test: To test all functions**
 
-3. npm test: To run test.**
-
-4. npm start: To run the script.**
+3. npm start: To run all the functions**
 
 ** **Warning: Display will go to sleep due to the sleep function**
--- test/addon.test.js
+++ test/addon.test.js
@@ -1,21 +1,26 @@
 let addon = require("../src/addon.js")
 
-//Enumerate the attached displays
-test("Test 1: Expect function to return the number of attached displays.", () => {
+//Test to check if display function returns number
+test("Test 1: Expect display function to return the number of attached displays.", () => {
 	expect(addon.display()).toEqual(expect.any(Number));
 });
 
+//Test to check if display function retuns number greater then 0
+test("Test 2: Expect display function to return a number greater then 0.", () => {
+	expect(addon.display()).toBeGreaterThan(0);
+});
+
 //Request the displays to sleep
-test("Test 2: Expect the function to return true to make sure it passed.", () => {
+test("Test 3: Expect sleep function to return true to make sure it passed.", () => {
 	expect(addon.sleep()).toBe(true);
 });
 
 //Request the displays to wake
-test("Test 3: Expect the function to return true to make sure it passed.", () => {
+test("Test 4: Expect wake function to return true to make sure it passed.", () => {
 	expect(addon.wake()).toBe(true);
 });
 
 //Return the last input time
-test("Test 4: Expect the function to return the last input time in seconds.", () => {
+test("Test 5: Expect lastInputTime function to return the last input time in seconds.", () => {
 	expect(addon.lastInputTime()).toEqual(expect.any(Number));
 });
